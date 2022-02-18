a952b44fa7170c5bd858a6119266dc5202c58f34
Bruno Cabral <cabral.brc@gmail.com>
2022-02-17 20:56:25 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+import pygame
+import sys
+from random import randint
+
+
+class Tree(pygame.sprite.Sprite):
+    def __init__(self, pos, group):
+        super().__init__(group)
+        self.image = pygame.image.load('graphics/tree.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, group):
+        super().__init__(group)
+        self.image = pygame.image.load('graphics/player.png').convert_alpha()
+        self.rect = self.image.get_rect(center=pos)
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        self.input()
+        self.rect.center += self.direction * self.speed
+
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+
+# setup
+camera_group = CameraGroup()
+player = Player((640, 360), camera_group)
+
+for i in range(20):
+    random_x = randint(0, 1000)
+    random_y = randint(0, 1000)
+    Tree((random_x, random_y), camera_group)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+    screen.fill('#71ddee')
+
+    camera_group.update()
+    camera_group.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
